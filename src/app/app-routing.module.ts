3362948c5a4c550b3bc8005f8a36ab29e3a4c5d4
fabiooliveirajjd <fabiooliveirajjd@gmail.com>
2022-11-28 15:40:23 -0300

    Rota para Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { Tecnico } from './models/tecnico';
-import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
-import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
-import { NgModule, Component } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 const routes: Routes = [
@@ -12,7 +12,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent }, 
   { path: 'tecnicos', component: TecnicoListComponent },
   { path: 'tecnicos/create', component: TecnicoCreateComponent },
-  { path: 'tecnicos/update', component: TecnicoUpdateComponent }
+  { path: 'tecnicos/update/:idTecnico', component: TecnicoUpdateComponent }
   ] }
 ];
 
