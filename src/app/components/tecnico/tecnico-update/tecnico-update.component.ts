2413e0da5254a04172413febe83d01cd104395d1
fabiooliveirajjd <fabiooliveirajjd@gmail.com>
2022-11-28 16:15:18 -0300

    Update Implementado

--- src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
+++ src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -1,5 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 @Component({
   selector: 'app-tecnico-update',
   templateUrl: './tecnico-update.component.html',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoUpdateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+ 
+  tecnico: Tecnico = {
+    idTecnico: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    dataCriacao: undefined
   }
 
-}
+  nome: FormControl =  new FormControl(null, Validators.minLength(3));
+  cpf: FormControl =       new FormControl(null, Validators.required);
+  email: FormControl =        new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
+
+  constructor(
+    private service: TecnicoService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+  this.tecnico.idTecnico = this.route.snapshot.paramMap.get('idTecnico');
+  this.findById();
+  }
+
+  validaCampos(): boolean {
+    return this.nome.valid && this.cpf.valid
+     && this.email.valid && this.senha.valid
+  }
+
+  findById(): void{
+    this.service.findById(this.tecnico.idTecnico).subscribe(resposta => {
+      this.tecnico = resposta;
+  
+    })
+  }
+
+
+  update(): void {
+    this.service.update(this.tecnico).subscribe(() =>{
+    this.router.navigate(['/tecnicos'])  
+    })
+    }
+  }
+ 
+
--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -11,6 +11,11 @@ export class TecnicoService {
 
   constructor(private http: HttpClient) { }
 
+
+  findById(idTecnico: any):Observable<Tecnico> {
+    return this.http.get<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos/${idTecnico}`);
+  }
+
   findAll(): Observable<Tecnico[]> {
     return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`);
   }
@@ -18,5 +23,7 @@ export class TecnicoService {
    create(tecnico: Tecnico): Observable<Tecnico> {
     return this.http.post<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos`,tecnico);
    }
-
+   update(tecnico: Tecnico): Observable<Tecnico> {
+    return this.http.put<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos/${tecnico.idTecnico}`,tecnico);
+   }
 }
