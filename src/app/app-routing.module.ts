bb20d593184b1abcfb9fa40a86d6487ae536df54
fabiooliveirajjd <fabiooliveirajjd@gmail.com>
2022-11-29 09:17:58 -0300

    componente cliente-update finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
@@ -19,8 +20,9 @@ const routes: Routes = [
   { path: 'tecnicos/delete/:idTecnico', component: TecnicoDeleteComponent },
 
   { path: 'clientes', component: ClienteListComponent },
-  { path: 'clientes/create', component: ClienteCreateComponent }
-  
+  { path: 'clientes/create', component: ClienteCreateComponent },
+  { path: 'clientes/update/:idCliente', component: ClienteUpdateComponent },
+  { path: 'tecnicos/delete/:idCliente', component: ClienteUpdateComponent }
   ] }
 ];
 
--- src/app/components/cliente/cliente-update/cliente-update.component.ts
+++ src/app/components/cliente/cliente-update/cliente-update.component.ts
@@ -1,5 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
 @Component({
   selector: 'app-cliente-update',
   templateUrl: './cliente-update.component.html',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteUpdateComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  cliente: Cliente = {
+    idCliente: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    dataCriacao: undefined
   }
 
-}
+  nome: FormControl =  new FormControl(null, Validators.minLength(3));
+  cpf: FormControl =       new FormControl(null, Validators.required);
+  email: FormControl =        new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
+
+  constructor(
+    private service: ClienteService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+  this.cliente.idCliente = this.route.snapshot.paramMap.get('idCliente');
+  this.findById();
+  }
+
+  validaCampos(): boolean {
+    return this.nome.valid && this.cpf.valid
+     && this.email.valid && this.senha.valid
+  }
+
+  findById(): void{
+    this.service.findById(this.cliente.idCliente).subscribe(resposta => {
+      this.cliente = resposta;
+  
+    })
+  }
+  update(): void {
+    this.service.update(this.cliente).subscribe(() =>{
+    this.router.navigate(['/clientes'])  
+    })
+    }
+  }
