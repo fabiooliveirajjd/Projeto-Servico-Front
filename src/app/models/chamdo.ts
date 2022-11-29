a9005d86296f2cd9f654fbd692cfab8f0be75e40
fabiooliveirajjd <fabiooliveirajjd@gmail.com>
2022-11-29 12:26:44 -0300

    Interface chamado criada.

---
+++ src/app/models/chamdo.ts
@@ -0,0 +1,14 @@
+export interface Chamado{
+    idChamado?: any;
+    dataAbertura: string;
+    dataFechamento: string;
+    prioridade: string;
+    status: string;
+    observacoes: string;
+    titulo: string;
+    tecnico: any;
+    cliente: any;
+    nomeCliente: string;
+    nomeTecnico: string;
+    valor: any;
+}
