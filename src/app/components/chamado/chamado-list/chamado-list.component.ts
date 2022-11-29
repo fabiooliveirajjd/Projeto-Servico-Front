e13fcf4f07991cbbf6ac0f2b910d5924f5ce068c
fabiooliveirajjd <fabiooliveirajjd@gmail.com>
2022-11-29 15:44:32 -0300

    HTML E CSS PARA CHAMADO-LIST

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,8 +1,8 @@
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { Chamado } from './../../../models/chamado';
-
+import { ChamadoService } from 'src/app/services/chamado.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Chamado } from  'src/app/models/chamado';
 
 @Component({
   selector: 'app-chamado-list',
@@ -10,39 +10,36 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./chamado-list.component.css']
 })
 export class ChamadoListComponent implements OnInit {
+  [x: string]: any;
   
-  ELEMENT_DATA: Chamado[] = [
-    {
-      idChamado: 1,
-      prioridade: 'ALTA',
-      status: 'ABERTO',
-      observacoes: 'Chamado Iniciado',
-      titulo: 'Chamado 1',
-      tecnico: '1',
-      cliente: '4',
-      nomeCliente: 'fabio',
-      nomeTecnico: 'Alan',
-      valor: '200',
-      dataAbertura: '29/11/2022',
-      dataFechamento: '29/22/2022'
-    }
-  ]
+  ELEMENT_DATA: Chamado[] = []
   
-  displayedColumns: string[] = ['idChamado', 'nomeTecnico', 'nomeCliente', 'prioridade', 'status',  'dataAbertura',
+  displayedColumns: string[] = ['idChamado', 'nomeTecnico', 
+  'nomeCliente', 'prioridade', 'status',  'dataAbertura',
   'dataFechamento', 'titulo', 'valor','acoes'];
   dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
   
   @ViewChild (MatPaginator) paginator: MatPaginator;
 
 
-  constructor() { }
+  constructor(
+    private service: ChamadoService
+  ) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
-
-    applyFilter(event: Event) {
+  
+  findAll(): void{
+    this.service.findAll().subscribe(resposta => {
+    this.ELEMENT_DATA = resposta;
+    this.dataSource = new MatTableDataSource<Chamado>(resposta);
+    })
+  }
+  
+  applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSource.paginator = this.paginator;
   }
-
 }
--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { API_CONFIG } from '../config/api.config';
 import { Tecnico } from '../models/tecnico';
 
