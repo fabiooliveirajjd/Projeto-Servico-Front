8851f4f8bd565e4d7325c276d37323b509b87a5d
fabiooliveirajjd <fabiooliveirajjd@gmail.com>
2022-11-30 14:36:29 -0300

    hTML E CSS PARA CHAMADO-CREATE FINALIZADO

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -11,7 +11,7 @@ export class NavComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/create'])
+    this.router.navigate(['chamado/create'])
   }
 
 }
